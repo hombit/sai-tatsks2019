748807f3921180cc5d52768b98adce8510a5a4d1
SergeyZHR <sergei.jeltouhov@yandex.ru>
2019-11-23 22:26:42 +0300

    исправлено

--- weather_parse.py
+++ weather_parse.py
@@ -1,80 +1,110 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 import requests
-import pickle
 import os
 from datetime import datetime
 import socket
-#cron 
-#1 0,3,6,9,12,15,18,21 * * * .../weather_parse.py
+from urllib.parse import urljoin
+import json
 
-#testing dark sky forecast data
-#Почему darksky?
-#мне удалось найти два сервиса, согласных бесплатно отдавать хоть чуть-чуть релевантное по API
-#darksky и weatherbit.io Оба выдают почасовое усреднение(свое) gfs и icon (Это две основные бесплатные модели)
-#weather underground закрыл api год назад. Да и ну их, с proprietary forecasting system BestForecast(tm)
+KGO_ADDR = ('37.29.115.163',16303)
+MAGIC_COMMAND = '1 get data\n'
+
+def Kgo_Meteo_Get_Data(only_one_flag = True):
+	try:
+		sock = socket.socket()
+		sock.settimeout(0.5)
+		sock.connect(KGO_ADDR)			#сокет сервер метеостанции КГО
+		sock.send(MAGIC_COMMAND.encode('ascii'))
+		data = sock.recv(1024).decode()[5:].split()
+		sock.close()
+		return data
+	except socket.timeout:
+		if only_one_flag: 
+			print('timeout error Meteo')
+			return Kgo_Meteo_Get_Data(False)
+	except socket.error as e:
+		print('SOCKET ERROR')
+		print(str(e))
+
+
+def Meteo_Parse_data(data):
+	if data is None:
+		return {}
+
+	kgo_vailasa={}
+	for p in data:
+		tmp = p.split('=')
+		try:
+			kgo_vailasa[tmp[0]]=float(tmp[1])		
+		except ValueError:
+			kgo_vailasa[tmp[0]]=None
+	return kgo_vailasa
+
+def Darksky_Data(only_one_flag = True):
+	with open('key.txt') as f:					#read api key
+		api_key = str(f.readline()[:-1])
+
+	url=urljoin(urljoin('https://api.darksky.net/forecast/',api_key+'/'),lat+','+lon)			#api ссылка
+
+	querystring = {"lang":"en","units":"si","exclude":"minutely,daily,alerts,flags"}		#выбираем только нужные прогнозы
+	response = requests.request("GET", url, params=querystring,timeout=(3,3))
+	
+	try:
+		if response.status_code == 200:
+			resp_dict_current = response.json()['currently']
+			resp_dict_forecast = response.json()['hourly']	
+		else:	
+			resp_dict_current = {}
+			resp_dict_forecast= {}
+
+	except requests.exceptions.ConnectTimeout :
+		if only_one_flag:
+			print('timeout error Darksky')
+			return Darksky_Data(False)
+
+	except requests.exceptions.ConnectionError:
+		resp_dict_current = {}
+		resp_dict_forecast= {}
+
+	return resp_dict_current, resp_dict_forecast 
+
+def main(city,lat,lon):
+	kgo_vailasa = Meteo_Parse_data(Kgo_Meteo_Get_Data())
+	resp_dict_current, resp_dict_forecast = Darksky_Data()
+
+	now = datetime.now()
+	now = now.replace(second = 0)
+	now = now.replace(minute=0)
+	now = now.replace(microsecond = 0)				#округляем время
+
+	ontimeDict = {'time':now.isoformat(),'current':resp_dict_current, 'forecast':resp_dict_forecast, 'meteo':kgo_vailasa}		#Создаем словарь c прогнозами
+	print(now)
+	print(city,'darksky current weather',resp_dict_current)
+	#print(now,city,resp_dict_forecast)
+	print(city,'meteostation',kgo_vailasa)
+
+	path=os.path.dirname(os.path.abspath(__file__) )
+
+	filename = os.path.join(path,'weather.dat')
+	if os.path.exists(filename):
+		with open(filename,"r") as file:			#читаем из файла то что там было
+			Wdat = json.load(file)
+	else:
+		Wdat={}
+
+	Wdat[now.isoformat()]=ontimeDict
+	with open(filename,"w") as file:			#пишем в файл для дальнейшей обработки
+		json.dump(Wdat,file)	
+
+
 
 city='KGO'
 lat='43.74611'		
 lon='42.6675'
 
-def kgo_METEO():
-	try:
-		sock = socket.socket()
-		sock.settimeout(0.5)
-		sock.connect(('37.29.115.163', 16303))			#сокет сервер метеостанции КГО
-		sock.send('1 get data\n'.encode('ascii'))
-		data = sock.recv(1024).decode()[5:].split()
-		sock.close()
-		kgo_vailasa={}
-		for p in data:
-			tmp = p.split('=')
-			try:
-				kgo_vailasa[tmp[0]]=float(tmp[1])		
-			except:
-				if len(tmp)==2:						#на всякий случай, что бы одно кривое поле не обвалило все данные с метеостанции
-					kgo_vailasa[tmp[0]]=tmp[1]
-				else:
-					kgo_vailasa[tmp[0]]=None
-	except Exception as e:
-		print('SOCKET ERROR')
-		print(str(e))
-		kgo_vailasa={}
-	return kgo_vailasa
-
-with open('key.txt') as f:					#read api key
-	api_key = str(f.readline()[:-1])
-
-url='https://api.darksky.net/forecast/'+api_key+'/'+lat+','+lon				#api ссылка
-
-querystring = {"lang":"en","units":"si","exclude":"minutely,daily,alerts,flags"}		#выбираем только нужные прогнозы
-try:
-	response = requests.request("GET", url, params=querystring)
-	resp_dict_current = response.json()['currently']
-	resp_dict_forecast = response.json()['hourly']		
-except Exception as e:
-	print(str(e))
-	resp_dict_current = {}
-	resp_dict_forecast={}
-
-kgo_vailasa = kgo_METEO()
-
-now = datetime.now()
-now = now.replace(second = 0)
-now = now.replace(minute=0)
-now = now.replace(microsecond = 0)				#округляем время
-
-ontimeDict = {'time':now,'current':resp_dict_current, 'forecast':resp_dict_forecast, 'meteo':kgo_vailasa}		#Создаем словарь c прогнозами
-print(now)
-print(city,'darksky current weather',resp_dict_current)
-#print(now,city,resp_dict_forecast)
-print(city,'meteostation',kgo_vailasa)
-
-path=os.path.dirname(os.path.abspath(__file__) )+'/'
-
-with open(path+now.strftime('%Y_%m_%dT%H_%M_%S')+'.pkl',"wb") as f:			#пишем в файл для дальнейшей обработки
-	pickle.dump(ontimeDict,f)
-
+if __name__ == "__main__":
+	main(city,lat,lon)
 
 #writing test
 # with open('2019_11_19T19_00_00.pkl',"rb") as f:
