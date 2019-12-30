0c5186ad75fa2f4ebbf212f8d566db3488203f1a
Kirill_Grishin <grischin.ka15@physics.msu.ru>
2019-12-30 13:59:26 +0300

    str.join()

--- kgrishin/make_tbl.py
+++ kgrishin/make_tbl.py
@@ -2,7 +2,9 @@
 You need to have API key in ~/.ads/dev_key
 This script uses ads python package: https://ads.readthedocs.io/en/latest/
 Name of environment variable is ADS_DEV_KEY
+You need to have ads package installed
 """
+
 import ads
 import datetime
 
@@ -16,14 +18,11 @@ def author_format(a_l):
 		fs = a_l[0] + ' et al.'
 	return fs
 
-def return_ads_sai():
-	now = datetime.datetime.now()
+def return_ads_sai(now):
 	papers = ads.SearchQuery(q='aff: ("SAI MSU" or "Sternberg") pubdate:[%i-%i]' % (now.year, now.month), sort="citation_count")
-	f = ""
-	for paper in papers:
-		f = f+ '<tr><td>%s</td><td>%s</td></tr>\n' % (paper.title[0], author_format(paper.author))
+	f = "\n".join('<tr><td>%s</td><td>%s</td></tr>' % (paper.title[0], author_format(paper.author)) for paper in papers)
 	return f
 
 if __name__ == '__main__':
-	r = return_ads_sai()
+	r = return_ads_sai(datetime.datetime.now())
 	print(r)
