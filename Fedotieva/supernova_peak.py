e8f883f29aedfc71dc360d5d13154ba377d53247
FedotievaAA <56800637+FedotievaAA@users.noreply.github.com>
2020-01-11 23:54:01 +0300

    Add files via upload

--- Fedotieva/supernova_peak.py
+++ Fedotieva/supernova_peak.py
@@ -1,9 +1,30 @@
 import requests
-from requests.exceptions import HTTPError
 import json
 import numpy as np
 import argparse
 import julian
+
+#Function shows data sources
+def print_source(source, alias, name, reference, bibcode, url):
+	print('Source(s): ')
+	source_s = source.split(',')
+	for s in source_s:
+		i, = np.where(alias==s)
+		i = i[0]
+		source_list = []
+		if name[i] != "no name":
+			source_list.append(name[i])
+		if reference[i] != "no reference":
+			source_list.append(reference[i])
+		if bibcode[i] != "no bibcode":
+			source_list.append(bibcode[i])
+		if url[i] != "no url":
+			source_list.append(url[i])
+
+		print(source_list)
+
+
+
 
 #Parse command line arguments
 parser = argparse.ArgumentParser()
@@ -15,11 +36,24 @@ SN_name = parser_args.name
 try:
 	response = requests.get('https://sne.space/astrocats/astrocats/supernovae/output/json/'+SN_name+'.json')
 	response.raise_for_status()
-except HTTPError:
-	print('Supernova '+SN_name+' is not found')
+except Exception as e:
+	print(e)
 	raise SystemExit
 
 data = response.json()
+
+#Get information about maximum from output data
+
+maxappmag = data[SN_name]['maxappmag'][0]['value']
+maxappmag_source = data[SN_name]['maxappmag'][0]['source']
+maxband = data[SN_name]['maxband'][0]['value']
+maxdate = data[SN_name]['maxdate'][0]['value']
+
+maxvisualappmag = data[SN_name]['maxvisualappmag'][0]['value']
+maxvisualappmag_source = data[SN_name]['maxvisualappmag'][0]['source']
+maxvisualband = data[SN_name]['maxvisualband'][0]['value']
+maxvisualdate = data[SN_name]['maxvisualdate'][0]['value']
+
 
 #Get photometry and source information from json
 photometry_list = data[SN_name]['photometry']
@@ -67,13 +101,7 @@ for element in sources_list:
 	alias.append(element['alias'])
 
 #Find available bands
-band_names = []
-
-for b in band:
-	if b in band_names:
-		continue
-	else:
-		band_names.append(b)
+band_names = set(band)
 
 time = np.asarray(time)
 mag = np.asarray(mag)
@@ -84,34 +112,33 @@ bibcode = np.asarray(bibcode)
 reference = np.asarray(reference)
 alias = np.asarray(alias)
 
-#Output information
+#Output data information
+
+print(f'max {maxband} band value\t{maxappmag}')
+print(f'max date\t{maxdate}')
+print_source(maxappmag_source, alias, name, reference, bibcode, url)
+print('\n')
+
+print(f'max {maxvisualband} band value\t{maxvisualappmag}')
+print(f'max date\t{maxvisualdate}')
+print_source(maxvisualappmag_source, alias, name, reference, bibcode, url)
+print('\n')
+
+
+
+#Derived information
 for b in band_names:
 	ind = band==b
 	time_ = time[ind]
 	mag_ = mag[ind]
 	source_ = source[ind]
 	i_max = np.argmin(mag_)
-	print(b+' band')
-	print('MJD max\t', time_[i_max])
-	print('Time max\t', julian.from_jd(time_[i_max], fmt='mjd'))
-	print(b+' value max\t', mag_[i_max])
-	print('Source(s): ')
-	source_s = source_[i_max].split(',')
-	for s in source_s:
-		i, = np.where(alias==s)
-		i = i[0]
-		source_list = []
-		if name[i] != "no name":
-			source_list.append(name[i])
-		if reference[i] != "no reference":
-			source_list.append(reference[i])
-		if bibcode[i] != "no bibcode":
-			source_list.append(bibcode[i])
-		if url[i] != "no url":
-			source_list.append(url[i])
-
-		print(source_list)
-
+	print(f'{b} band')
+	print(f'MJD max\t{time_[i_max]}')
+	date = julian.from_jd(time_[i_max], fmt='mjd')
+	print(f'Time max\t{date}')
+	print(f'{b} value max\t{mag_[i_max]}')
+	print_source(source_[i_max], alias, name, reference, bibcode, url)
 		
 	print('\n')
 
