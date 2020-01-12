4f7994ccc22eefa5262148dbc721b5d96045adf3
kotOcelot <56804525+kotOcelot@users.noreply.github.com>
2020-01-12 15:59:28 +0300

    Update ztf.py

--- Podzolkova/ztf.py
+++ Podzolkova/ztf.py
@@ -4,66 +4,59 @@ import numpy as np
 from io import BytesIO
 from astropy.io.votable import parse_single_table
 from astropy.table import Table
+import sys
 
-def main():
-    print('Ввести координаты (y) или использовать по умолчанию (n)?')
-    ans = input()
-    if ans == 'n':
+def create_table(bandname, coord):
+    response = requests.get('https://irsa.ipac.caltech.edu/cgi-bin/ZTF/nph_light_curves?POS=CIRCLE {0[0]} {0[1]} 0.0014&BANDNAME={1[0]}'.format(coord, bandname))
+    if response.status_code == 200:
+        print('OK')
+        res = response.content
+        obj = BytesIO(res)
+        table = parse_single_table(obj)
+        data = Table(table.array) #Полученная кривая блеска
+        return(data)
+    else:
+        print('err: ' + str(response.status_code))
+            
+def calc_mean(data):    
+    if (len(data) != 0): #Может не быть объекта по заданным координатам
+        objects = np.unique(np.array(data['oid'])) #Cписок ID всех объектов
+        mag = np.array(data['mag']) #Нужные столбцы
+        magerr = np.array(data['magerr'])
+        for k in objects: #Отсеиваем объекты со слишком малым числом точек по ID
+            ind_start = np.where(np.array(data['oid']) == k)[0][0] #Начало и конец соответствующих одному объекту измерений
+            ind_fin = np.where(np.array(data['oid']) == k)[0][-1]
+            d_array = np.where(data['oid'] == k)[0] #Список индексов
+            if ind_fin - ind_start < 25: #Удаляем
+                mag = np.delete(mag, d_array)
+                magerr = np.delete(magerr, d_array)   
+        b_mag = np.sum(mag/((magerr)**2))/np.sum(1/((magerr)**2)) #Считаем среднее
+        err = np.sqrt(1/(np.sum(1/((magerr)**2))))
+        return(b_mag, err)
+    else:
+        print('Нет объекта с такими координатами!')
+        
+def calc(bandname, coord):
+    table = create_table(bandname, coord)
+    b_mag, err = calc_mean(table)
+    return(b_mag, err)
+
+def main():   
+    if len(sys.argv) == 1:
         RA = 298.0025
         DEC = 29.87147
-    elif ans == 'y': 
-        print('Введите RA, DEC:')
-        RA, DEC = map(float, input().split(', '))
-        
-    response_g = requests.get('https://irsa.ipac.caltech.edu/cgi-bin/ZTF/nph_light_curves?POS=CIRCLE ' + str(RA) + ' ' + str(DEC) + ' 0.0014&BANDNAME=g')
-    if response_g.status_code == 200:
-        print('OK')
-        res_g = response_g.content #А json не работает
-        obj_g = BytesIO(res_g)
-        table_g = parse_single_table(obj_g) #А votable просит только файлы
-        data_g = Table(table_g.array)
-        if (len(data_g) != 0):
-            objects_g = np.unique(np.array(data_g['oid']))
-            mag_g = np.array(data_g['mag'])
-            magerr_g = np.array(data_g['magerr'])
-            for k in objects_g:
-                if np.where(np.array(data_g['oid']) == k)[0][-1] - np.where(np.array(data_g['oid']) == k)[0][0] < 100:
-                    mag_g = np.delete(mag_g, np.where(data_g['oid'] == k)[0])
-                    magerr_g = np.delete(magerr_g, np.where(data_g['oid'] == k)[0])   
-            g = np.sum(mag_g/((magerr_g)**2))/np.sum(1/((magerr_g)**2))
-            gerr = np.sqrt(1/(np.sum(1/((magerr_g)**2))))
-            response_r = requests.get('https://irsa.ipac.caltech.edu/cgi-bin/ZTF/nph_light_curves?POS=CIRCLE ' + str(RA) + ' ' + str(DEC) + ' 0.0014&BANDNAME=r')
-            if response_g.status_code == 200:
-                print('OK')
-                res_r = response_r.content
-                obj_r = BytesIO(res_r)
-                table_r = parse_single_table(obj_r)
-                data_r = Table(table_r.array)
-                if (len(data_r) != 0):
-                    objects_r = np.unique(np.array(data_r['oid']))
-                    mag_r = np.array(data_r['mag'])
-                    magerr_r = np.array(data_r['magerr'])
-                    for k in objects_r:
-                        if np.where(np.array(data_r['oid']) == k)[0][-1] - np.where(np.array(data_r['oid']) == k)[0][0] < 100:
-                            mag_r = np.delete(mag_r, np.where(data_r['oid'] == k)[0])
-                            magerr_r = np.delete(magerr_r, np.where(data_r['oid'] == k)[0]) 
-                    r = np.sum(mag_r/((magerr_r)**2))/np.sum(1/((magerr_r)**2))
-                    rerr = np.sqrt(1/(np.sum(1/((magerr_r)**2))))
-                    gr = g-r
-                    grerr = np.sqrt(gerr**2 + rerr**2)
-                    print('g - r: ' + str(gr) + ' +- ' + str(grerr))
-                else:
-                    print('В r нет объекта с такими координатами!')
-            else:
-                print('err: ' + str(response_g.status_code))
-        else:
-            print('В g нет объекта с такими координатами!')
-    else:
-        print('err: ' + str(response_g.status_code))
-
-      
+    elif len(sys.argv) == 2: 
+        RA, DEC = sys.argv[1], sys.argv[2]
+    coord = (RA, DEC)
+    r_mag, rerr = calc('r', coord)
+    g_mag, gerr = calc('g', coord)
+    gr = g_mag - r_mag
+    grerr = np.sqrt(gerr**2 + rerr**2)
+    print('g - r: ' + str(gr) + ' +- ' + str(grerr))    
+    
 if __name__ == '__main__':
     main()
     
+
 
 
