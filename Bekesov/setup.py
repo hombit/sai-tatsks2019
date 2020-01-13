9b7983a5507f9ffd0fd5a7fa6f2a15b1c5ee4db0
Konstantin Malanchev <hombit@gmail.com>
2020-01-13 15:09:05 +0300

    Add docker to Bekesov

--- Bekesov/setup.py
+++ Bekesov/setup.py
@@ -5,7 +5,7 @@ setup(
     name='OGLI',
     version='1.0',
     test_suite='Test',
-    scripts = ["OGlI"],
-    install_requires=['requests', 'sys'],
+    scripts = ["OGLI.py"],
+    install_requires=['requests'],
     long_description=open(join(dirname(__file__), 'README.txt')).read(),
 )
