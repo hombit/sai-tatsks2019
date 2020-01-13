18fb1260056caefae854bda8b429f06f23aa6216
mayamolodec <maya.molodec@gmail.com>
2020-01-13 14:24:34 +0300

    Update views.py
    Теперь тут выводится выоста звезды и солнца на заданную дату

--- Voloshina/kgo/stars/views.py
+++ Voloshina/kgo/stars/views.py
@@ -18,7 +18,9 @@ import matplotlib.dates as mdates
 from matplotlib import rc, rcParams
 import matplotlib.font_manager
 import numpy as np
-
+import math
+import django.utils
+from django.http import HttpResponseNotFound
 
 def index(request):
     if request.method == 'POST':
@@ -28,17 +30,19 @@ def index(request):
             'Times':request.POST["Times"],
             'image': "data:image/png;base64,"+ base64.b64encode(createPicture(request.POST["Name"], request.POST["Times"])).decode("UTF-8")
             }
-        except:
-            content={
-            "Error":"404 NOT FOUND YOUR STAR"
-            }
+
+        except astropy.coordinates.name_resolve.NameResolveError:
+            return HttpResponseNotFound("NOT RESOLVED YOUR STAR")
+
+        except ValueError:
+            return HttpResponseNotFound("INVALID VALUE (PRESUMABLY FOR DATE)")
+
         return render(request, 'stars/index.html', content)
     return render(request, 'stars/index.html')
 
 
 
 def createPicture(name, times):
-    print(type(times))
     rcParams['font.size'] = 13
     rcParams['lines.linewidth'] = 3
     rcParams['lines.markersize'] = 0
@@ -60,44 +64,45 @@ def createPicture(name, times):
     star_style = {'linestyle': '--', 'linewidth': 4, 'color': 'tomato'}
     star = FixedTarget.from_name(star_name)
 
-    kgo = Observer(longitude=37.5425*u.deg, latitude=55.701*u.deg,
-                      elevation=194*u.m, name="KGO", timezone="Europe/Moscow")
-                      #observe_time = Time('2000-01-13 03:00:00')
+    longitude_kgo = 42.6675*u.deg
+    latitude_kgo = 43.73611*u.deg
+    elevation_kgo = 2112*u.m
 
-    start_time = Time('2020-01-01 '+ times)
-    end_time = Time('2020-12-31 '+ times)
-    delta_t = end_time - start_time
-    observe_time = start_time + delta_t*np.linspace(0, 1, 366)
-    moon_coord = kgo.moon_altaz(observe_time)
-    star_coord = kgo.altaz(observe_time, star)
+    kgo = Observer(longitude=longitude_kgo, latitude=latitude_kgo,
+                      elevation=elevation_kgo, name="KGO", timezone="Europe/Moscow")
+
+    observe_time = Time(times)
+    visible_time = observe_time + np.linspace(-10, +10, 25)*u.hour
+    stars_alts = kgo.altaz(visible_time, star).alt
+    sun_alts = kgo.sun_altaz(visible_time).alt
+    
+    moon_coord = kgo.moon_altaz(visible_time)
+    star_coord = kgo.altaz(visible_time, star)
     angle = moon_coord.separation(star_coord)
     moon_star = angle.deg
+
+
     locator = mdates.MonthLocator() 
     fmt = mdates.DateFormatter('%b')
 
     canvas = FigureCanvasAgg(plt.figure(1))
     plt.figure(figsize=(8,7))
     plt.subplot(211)
-    plot_airmass(star, kgo, observe_time, style_kwargs=star_style, altitude_yaxis=True)
-
-    X = plt.gca().xaxis
-    X.set_major_locator(locator)
-    X.set_major_formatter(fmt)
+    plt.plot_date(visible_time.plot_date, stars_alts, linestyle = '-.', color = 'mediumslateblue', label = star_name)
+    plt.plot_date(visible_time.plot_date, sun_alts, linestyle = '-.', color = 'gold', label = 'Sun')
+    plt.ylim(0, np.max([stars_alts, sun_alts]) + 5)
+    plt.ylabel('Altitude, degrees')
+    plt.legend(shadow=True, loc="best")
     plt.gcf().autofmt_xdate() 
-    plt.tight_layout()
     plt.grid()
 
     plt.subplot(212)
-    t = Time(observe_time, format='iso', scale='utc')
-    plt.plot_date(t.plot_date, moon_star, linestyle = '-', color = 'mediumseagreen', label = star_name+'\n'+times)
-    plt.xlim(t.plot_date[0], t.plot_date[-1])
+    plt.plot_date(visible_time.plot_date, moon_star, linestyle = '-', color = 'slategrey')
     plt.ylabel('Moon-Star angle, degrees')
-    X = plt.gca().xaxis
-    X.set_major_locator(locator)
-    X.set_major_formatter(fmt)
+    plt.gcf().autofmt_xdate() 
     plt.grid()
-    plt.legend(shadow=True, loc=2)
     data = BytesIO()
 
     plt.savefig(data, format='png')
     return data.getvalue()
+
