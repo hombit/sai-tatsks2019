130d78264c63f37df560830a0d5e37b2725cb9b3
Alyona Balakina <elena@balakin.ru>
2020-01-13 04:21:12 +0300

    Update script

--- Balakina/ads_query.py
+++ Balakina/ads_query.py
@@ -38,34 +38,33 @@ def parser():
                         help='Max pages to seach, default 3. Uses API Key daily quota(max 5000/day)')
     parser.add_argument('-d', '--days', default=365, type=int,
                         help='Max days to search since today(backwards, default 365)')
-    parser.add_argument("-v", "--verbosity", default=False, action='store_true',
-                        help="increase output verbosity")
+
     return parser
 
 
 def main():
-    opts = parser().parse_args()
-    if not opts.keyword:
+    args = parser().parse_args()
+    if not args.keyword:
         print('No keyword, exiting')
         exit(1)
     dt_list = []
     now = datetime.datetime.now()
-    dt1 = (now - datetime.timedelta(days=opts.days)).strftime('%Y-%m')
+    dt1 = (now - datetime.timedelta(days=args.days)).strftime('%Y-%m')
     dt2 = now.strftime('%Y-%m')
     q = ads.SearchQuery(q='(abs:"{keyword}" or title:"{keyword}") '
-                        'database:astronomy property:refereed pubdate:[{dt1} TO {dt2}]'.format(keyword=opts.keyword, dt1=dt1, dt2=dt2),
-                        fl=['id', 'bibcode', 'title', 'citation_count', 'pubdate'], sort='pubdate desc', max_pages=opts.pages)
+                        'database:astronomy property:refereed pubdate:[{dt1} TO {dt2}]'.format(keyword=args.keyword, dt1=dt1, dt2=dt2),
+                        fl=['id', 'bibcode', 'title', 'citation_count', 'pubdate'], sort='pubdate desc', max_pages=args.pages)
     for paper in q:
-        if opts.verbosity:
-            print(paper.title, paper.pubdate)
         dt_list.append(datetime.datetime.strptime(paper.pubdate.replace('-00-', '-01-'), '%Y-%m-00'))
     x = np.array(dt_list)
     df = pd.DataFrame({'x': x.astype('datetime64')})
-    df.groupby([df['x'].dt.year, df['x'].dt.month]).count().plot(kind='bar', color='#4e79a7')
-    ax = plt.axes()
+    fig, ax = plt.subplots()
+    df.groupby([df['x'].dt.year, df['x'].dt.month]).count().plot(kind='bar', color='#4e79a7', ax=ax, legend=False)
+    fig.subplots_adjust(left=.15, bottom=.25, right=.95, top=.95)
     ax.yaxis.set_minor_locator(plt.MultipleLocator(1))
-    plt.xlabel('Month')
-    plt.legend(loc='upper right')
+    plt.xlabel('Date')
+    plt.ylabel('Number of papers')
+    plt.setp(ax.get_xticklabels(), rotation=55)
     plt.show()
 
 
