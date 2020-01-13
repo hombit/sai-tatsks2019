6b63e82ad92514634ed8546b232a22a4fa1893a0
Alyona Balakina <elena@balakin.ru>
2020-01-13 13:09:02 +0300

    Improve page search

--- Balakina/ads_query.py
+++ Balakina/ads_query.py
@@ -7,6 +7,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rc, rcParams
 import calendar
+import os
+import math
 
 rcParams['font.size'] = 14
 rcParams['xtick.direction'] = 'in'
@@ -23,40 +25,8 @@ rc('ytick.major', size=5, width=1)
 rc('xtick.minor', size=0)
 rc('ytick.minor', size=3, width=1)
 
-try:
-    with open('./apikey', 'r') as f:
-        ads.config.token = f.read().strip()
-except OSError:
-    print('No token, exiting')
-    exit(1)
 
-
-def parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('keyword', type=str,
-                        help='Keyword to search NASA ADS')
-    parser.add_argument('-p', '--pages', default=15, type=int,
-                        help='Max pages to seach, default 15. Uses API Key daily quota (max 5000/day)')
-    parser.add_argument('-d', '--days', default=365, type=int,
-                        help='Max days to search since today(backwards, default 365)')
-
-    return parser
-
-
-def main():
-    args = parser().parse_args()
-    if not args.keyword:
-        print('No keyword, exiting')
-        exit(1)
-    dt_list = []
-    now = datetime.datetime.now()
-    dt1 = (now - datetime.timedelta(days=args.days)).strftime('%Y-%m')
-    dt2 = now.strftime('%Y-%m')
-    q = ads.SearchQuery(q='(abs:"{keyword}" or title:"{keyword}") '
-                        'database:astronomy property:refereed pubdate:[{dt1} TO {dt2}]'.format(keyword=args.keyword, dt1=dt1, dt2=dt2),
-                        fl=['id', 'bibcode', 'title', 'citation_count', 'pubdate'], sort='pubdate desc', max_pages=args.pages)
-    for paper in q:
-        dt_list.append(datetime.datetime.strptime(paper.pubdate.replace('-00-', '-01-'), '%Y-%m-00'))
+def draw_plot(dt_list):
     x = np.array(dt_list)
     df = pd.DataFrame({'x': x.astype('datetime64')})
     fig, ax = plt.subplots()
@@ -64,12 +34,60 @@ def main():
     str_index = ['{month} {year}'.format(month=calendar.month_abbr[key[1]], year=key[0]) for key in group.index]
     group.plot(kind='bar', color='#4e79a7', ax=ax, legend=False)
     fig.subplots_adjust(left=.15, bottom=.25, right=.95, top=.95)
-    ax.yaxis.set_minor_locator(plt.MultipleLocator(1))
     plt.xlabel('Date')
     plt.ylabel('Number of papers')
     ax.set_xticklabels(str_index)
     plt.setp(ax.get_xticklabels(), rotation=55)
     plt.show()
+
+
+def read_apikey():
+    try:
+        ads.config.token = os.environ['ADS_DEV_KEY']
+    except KeyError:
+        try:
+            with open('./apikey', 'r') as f:
+                ads.config.token = f.read().strip()
+        except OSError:
+            print('No token, exiting')
+            exit(1)
+
+
+def parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('keyword', type=str,
+                        help='Keyword to search NASA ADS')
+    parser.add_argument('-d', '--days', default=365, type=int,
+                        help='Max days to search since today (backwards, default 365)')
+
+    return parser
+
+
+def append_next(q, l):
+    for paper in q:
+        l.append(datetime.datetime.strptime(paper.pubdate.replace('-00-', '-01-'), '%Y-%m-00'))
+
+
+def main():
+    read_apikey()
+    args = parser().parse_args()
+    dt_list = []
+    now = datetime.datetime.now()
+    dt1 = (now - datetime.timedelta(days=args.days)).strftime('%Y-%m')
+    dt2 = now.strftime('%Y-%m')
+    q = ads.SearchQuery(q='(abs:"{keyword}" or title:"{keyword}") '
+                        'database:astronomy property:refereed pubdate:[{dt1} TO {dt2}]'.format(keyword=args.keyword, dt1=dt1, dt2=dt2),
+                        fl=['id', 'bibcode', 'title', 'citation_count', 'pubdate'], sort='pubdate desc', rows=2000, max_pages=1)
+    q.execute()
+    num_found = q.response.numFound
+    need_pages = math.ceil(num_found / 2000)
+    if need_pages > 1:
+        q = ads.SearchQuery(q='(abs:"{keyword}" or title:"{keyword}") '
+                            'database:astronomy property:refereed pubdate:[{dt1} TO {dt2}]'.format(keyword=args.keyword, dt1=dt1, dt2=dt2),
+                            fl=['id', 'bibcode', 'title', 'citation_count', 'pubdate'], sort='pubdate desc', rows=2000, max_pages=need_pages)
+
+    append_next(q, dt_list)
+    draw_plot(dt_list)
 
 
 if __name__ == "__main__":
