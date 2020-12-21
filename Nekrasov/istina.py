e7c0113339ce8f991a9afa24171ef06a4ce416cc
Proton1904 <72749297+Proton1904@users.noreply.github.com>
2020-12-21 16:45:22 +0300

    Create istina.py

---
+++ Nekrasov/istina.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""Script to get list of articles written by SAI collaborators for the last month according to the ISTINA system
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    
+    return
+    
+if __name__ == '__main__':
+    main()
