1fc95689a931c5b0c370ea3172b52d888a1b27cb
AlexandraNovinskaya <40914706+AlexandraNovinskaya@users.noreply.github.com>
2021-01-04 17:28:38 +0300

    Update power.py

--- Novinskaya/power.py
+++ Novinskaya/power.py
@@ -1,41 +1,41 @@
 from influxdb import InfluxDBClient
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+from datetime import datetime
 
 
 #Пороговое значение напряжения
-voltage_threshold=10
+voltage_threshold = 10
 
+def power_kgo():
+    client = InfluxDBClient(host='eagle.sai.msu.ru',port=80,database='collectd')
 
-client = InfluxDBClient(host='eagle.sai.msu.ru',port=80,database='collectd')
+    #Задаем дату программно
+    begin_date = datetime(2020, 11, 30, 0, 0)
+    end_date = datetime(2020, 12, 30, 0, 0)
+    #end_date = datetime.now()
+     
+    begin_date_str = begin_date.strftime("%Y-%m-%dT%H:%M:%SZ")
+    end_date_str = end_date.strftime("%Y-%m-%dT%H:%M:%SZ")
+    
+    #Минимальные значения напряжения в определенный период
+    query = client.query('''select min(value) from snmp_value where \"type\"='voltage' and \"type_instance\"='input' and time >= '{}' and time<= '{}' group by time(1h)'''.format(begin_date_str,end_date_str), database='collectd')
+    
 
-#Минимальные значения напряжения в период с 30 ноября по 30 декабря
-query=client.query("select min(value) from snmp_value where \"type\"='voltage' and \"type_instance\"='input' and time >= '2020-11-30T00:00:00Z' and time<= '2020-12-30T23:59:59Z' group by time(1h)",database='collectd')
+    #Временные метки, формат
+    data = np.array(query.raw['series'][0]['values'])
+    dates = pd.to_datetime(data[:,0])
+    vals = data[:,1].astype(np.double)
 
-#Временные метки, формат
-data=np.array(query.raw['series'][0]['values'])
-dates=pd.to_datetime(data[:,0])
-vals=data[:,1].astype(np.double)
+    indx_less = np.argwhere(vals<voltage_threshold)
 
+    print('Number of power outages = {}'.format(len(indx_less)))
 
-with np.errstate(invalid='ignore'):
-    indx_less=np.argwhere(vals<voltage_threshold)
+    client.close()
+    
+    return
 
-
-#График зависимости напряжения от времени
-plt.plot(dates,vals)
-plt.xticks(rotation=25)
-plt.grid()
-plt.ylabel('Voltage')
-plt.xlabel('Data')
-plt.title('')
-plt.tick_params(axis='x', which='both', labelsize=8, direction = 'in')
-plt.tick_params(axis='y', which='both', labelsize=8, direction = 'in')
-plt.title('Voltage vs data')
-plt.show()
-
-
-print('Number of times when voltage was less than {} = {}'.format(voltage_threshold,len(indx_less)))
-
-client.close()
+if __name__ == '__main__':
+    
+    power_kgo()
+   
