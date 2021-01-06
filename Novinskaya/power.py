de7113818630215d98d105cbcb68aadf5d1a737d
AlexandraNovinskaya <40914706+AlexandraNovinskaya@users.noreply.github.com>
2021-01-06 12:59:01 +0300

    Update power.py

--- Novinskaya/power.py
+++ Novinskaya/power.py
@@ -1,8 +1,5 @@
 from influxdb import InfluxDBClient
 import numpy as np
-#from datetime import datetime, timedelta
-#import matplotlib.pyplot as plt
-
 from datetime import date
 from dateutil.relativedelta import relativedelta
 from itertools import groupby
@@ -13,13 +10,6 @@ VOLTAGE_THRESHOLD = 10
 
 def power_kgo():
     client = InfluxDBClient(host='eagle.sai.msu.ru',port=80,database='collectd')
-
-    #Минимальные значения напряжения в период с 30 ноября по 30 декабря
-    #begin_date = datetime(2020, 11, 30, 0, 0)
-    #end_date = datetime(2020, 12, 30, 0, 0)
-    
-    #begin_date = datetime.now()
-    #end_date = datetime.now() + timedelta(months=1)
     
     begin_date = date.today() + relativedelta(months=-1)
     end_date = date.today()
@@ -33,30 +23,14 @@ def power_kgo():
 
     #Временные метки, формат
     data = np.array(query.raw['series'][0]['values'])
-    #dates = pd.to_datetime(data[:,0])
     vals = data[:,1].astype(np.double)
-    #print(vals<VOLTAGE_THRESHOLD)
     
-
-
-
-    #with np.errstate(invalid='ignore'):
-    #indx_less = np.argwhere(vals<VOLTAGE_THRESHOLD)
-    
-    
-    #l=indx_less
-    #result= np.split(l, np.where(np.diff(l,axis=0)>1)[0]+1)
-
-    #print('Number of power outages = {}'.format(len(indx_less)))
-  
-    
-    #print('Number of power outages = {}'.format(sum(k for k,v in groupby(vals<VOLTAGE_THRESHOLD))))
     
     print('Number of power outages = {}'.format(np.sum(np.diff(np.concatenate([[0],vals<VOLTAGE_THRESHOLD,[0]]))>0)))
     
     client.close()
     
-    #return
+    
 
 if __name__ == '__main__':
     
